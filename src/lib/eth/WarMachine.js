acd552680926c06f7e6d52031897cc6040212c6b
orc-tzan <>
2021-05-11 10:36:04 -0300

    getWarReportwGOLD

--- src/lib/eth/WarMachine.js
+++ src/lib/eth/WarMachine.js
@@ -1,6 +1,7 @@
 import WarMachineABI from "./WarMachine.json";
 import { ethers } from "ethers";
 import { getTroops } from "@/data/Troops";
+import { getWars } from "@/data/Wars";
 import BigNumber from "bignumber.js";
 
 export default class WarMachine {
@@ -149,10 +150,21 @@ export default class WarMachine {
 
   async getWarReportwGOLD() {
     const stage = parseInt(await this.warStage());
-    let getTotalPrize = await this.smc.methods.getTotalPrize().call();
-    getTotalPrize = new BigNumber(getTotalPrize);
+    // let getTotalPrize = await this.smc.methods.getTotalPrize().call();
+    // getTotalPrize = new BigNumber(getTotalPrize);
+    const war = getWars.find(
+      (war) => war.contractAddress[[this.networkInfo] === this.contractAddress]
+    );
+    if (!war) {
+      return {
+        totalPrize: "0",
+        won: "0",
+        burned: "0",
+      };
+    }
+    let getTotalPrize = new BigNumber(war.totalPrize);
 
-    if (stage < 2) {
+    if (!war || stage < 2) {
       return {
         totalPrize: getTotalPrize.toFixed(0),
         won: "0",
