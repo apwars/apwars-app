8c4813ebcd725375917dd7aac29ddb445c1ae876
luthien-elve <luthien-elve@protonmail.com>
2021-09-06 10:15:21 -0300

    Adjusts in spacement in tab Weapons in war preparationa page

--- src/lib/components/ui/Units/UnitWarPreparation.vue
+++ src/lib/components/ui/Units/UnitWarPreparation.vue
@@ -35,15 +35,16 @@
         <div class="d-flex align-center my-1 qty">
           <img class="mr-1" height="32px" src="/images/icons/hourglass.png" />
 
-          <div class="d-flex d-flex align-items-center justify-content-justify">
-            <span class="mr-working-time">
-              Working time:
+          <div class="d-flex flex-column">
+            <span class="d-flex align-items-center justify-content-justify">
+              <span class="mr-working-time"> Working time:</span>
               <amount
                 :amount="getGeneralConfig.blocks"
                 decimals="0"
                 formatted
+                compact
               />
-              <span class="ml-blocks">blocks</span>
+              <span class="ml-blocks"> blocks </span>
             </span>
             <span><time-block :blocks="getGeneralConfig.blocks"/></span>
           </div>
