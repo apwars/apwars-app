45c19e5eb10e764ec382ef11e3dc35c9b6dbb323
dmenethil <90006599+dmenethil@users.noreply.github.com>
2021-09-09 13:59:30 -0300

    Fix items count display (#3)

--- src/pages/Inventory.vue
+++ src/pages/Inventory.vue
@@ -51,7 +51,7 @@
           <div class="d-flex">
             <img src="/images/black-market/book.png" class="resources" />
             <div class="text-center ml-2">
-              <h2>{{ totalItems }} / {{ collectibles.length }}</h2>
+              <h2>{{ itemsCount }} / {{ collectibles.length }}</h2>
               <h4>Collectibles</h4>
             </div>
           </div>
