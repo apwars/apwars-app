aad6c6f647a03614edaef06b89c1a6b1553054cf
orc-tzan <>
2021-09-09 19:14:39 -0300

    map

---
+++ src/lib/components/ui/Modals/WorkerModal.vue
@@ -0,0 +1,67 @@
+<template>
+  <div>
+    <template-modal-papyrus
+      :open="open"
+      :title="'Worker'"
+      @close="$emit('close')"
+      @confirm="$emit('confirm')"
+      :textConfirm="textConfirm"
+      :isLoading="isLoading"
+      :disabledConfirm="isLoading"
+      :disabledClose="isLoading"
+    >
+      <v-row dense class="d-flex">
+        <v-col cols="12" md="3" class="align-self-center">
+          <img class="worker-img" src="/Map/farm-worker/map/assets/worker.png" />
+        </v-col>
+        <v-col cols="12" md="9" class="d-flex align-self-center">
+          <p class="ml-2 mt-1 align-self-center content">
+            <v-progress-circular
+              class="mr-1"
+              v-if="isLoading"
+              :size="24"
+              :width="2"
+              indeterminate
+              color="secondary"
+            ></v-progress-circular>
+            <span v-html="text"></span>
+          </p>
+        </v-col>
+      </v-row>
+    </template-modal-papyrus>
+  </div>
+</template>
+
+<script>
+import wButton from "@/lib/components/ui/Buttons/wButton";
+import TemplateModalPapyrus from "@/lib/components/ui/Modals/Templates/TemplateModalPapyrus";
+
+export default {
+  props: ["open", "isLoading", "text", "textConfirm"],
+
+  components: {
+    wButton,
+    TemplateModalPapyrus,
+  },
+
+  methods: {
+    close() {
+      this.$emit("close");
+    },
+  },
+};
+</script>
+
+<style scoped>
+.worker-img {
+  width: 100%;
+}
+
+@media only screen and (max-width: 600px) {
+  .worker-img {
+    width: 80px;
+    margin: 0 auto;
+    display: block;
+  }
+}
+</style>
--- src/pages/FarmWorker.vue
+++ src/pages/FarmWorker.vue
@@ -1,15 +1,58 @@
 <template>
   <div>
-    <div id="farmWorker"></div>
+    <div id="farmWorker">
+      <div class="menuWorker">
+        <div v-if="isProgressWorker" class="d-flex align-center progressWorker">
+          <img
+            src="Map/farm-worker/map/assets/food_supplement.png"
+            alt="food supplement"
+          />
+          <v-progress-linear
+            color="#24B0FF"
+            background-color="#3A2720"
+            :value="percetageProgress"
+            height="30"
+          ></v-progress-linear>
+        </div>
+        <div
+          v-else-if="!isProgressWorker && !isClaim"
+          class="d-flex align-center justify-center"
+        >
+          <wButton @click="openModalWorker()">
+            Get Worker
+          </wButton>
+        </div>
+        <div v-else class="d-flex align-center justify-center">
+          <wButton @click="claim()">
+            Claim
+          </wButton>
+        </div>
+      </div>
+    </div>
+    <worker-modal
+      v-if="modalWorker"
+      :open="modalWorker"
+      @confirm="startWorker"
+      @close="modalWorker = false"
+      :isLoading="isLoadingWorker"
+      :text="
+        `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book`
+      "
+    ></worker-modal>
   </div>
 </template>
 
 <script>
 import * as PIXI from "pixi.js";
 import * as TRAVISO from "traviso.js";
+import wButton from "@/lib/components/ui/Buttons/wButton";
+import WorkerModal from "@/lib/components/ui/Modals/WorkerModal";
 
 export default {
-  components: {},
+  components: {
+    wButton,
+    WorkerModal,
+  },
 
   data() {
     return {
@@ -17,6 +60,15 @@ export default {
       isRenderFarm: false,
       engine: {},
       pixiRoot: {},
+      stageWork: 0,
+      isLoopWork: true,
+      stepSpeed: 1,
+      worker: null,
+      isProgressWorker: false,
+      isClaim: false,
+      modalWorker: false,
+      isLoadingWorker: false,
+      percetageProgress: 0,
     };
   },
 
@@ -73,45 +125,89 @@ export default {
 
       var instanceConfig = {
         mapDataPath: "./Map/farm-worker/mapData.json",
-        tileHeight: 71,
-        isoAngle: 30,
+        tileHeight: 142,
+        isoAngle: 29,
+        changeTransparencies: false,
         highlightPath: false,
-        mapDraggable: false,
+        mapDraggable: true,
         highlightTargetTile: false,
         followCharacter: false,
         checkPathOnEachTile: false,
+        minScale: 0.1,
+        initialZoomLevel: 0.1,
         engineInstanceReadyCallback: () => {
-          // this.engine.disableInteraction();
-          // this.startWorker();
-        }
+          this.engine.centralizeToLocation(2, 3, true);
+          this.engine.disableInteraction();
+          this.worker = this.engine.createAndAddObjectToLocation("9", {
+            c: 3,
+            r: 5,
+          });
+          this.engine.setCurrentControllable(this.worker);
+        },
+        objectReachedDestinationCallback: () => {
+          this.stageWork++;
+          if (this.stageWork === 4) {
+            this.worker.changeVisualToDirection(
+              TRAVISO.DIRECTIONS.NW,
+              false,
+              false,
+              false,
+              null,
+              2
+            );
+            this.stageWork = 0;
+          }
+          this.isLoopWork && this.moveWorker(this.stageWork);
+        },
       };
 
       this.engine = TRAVISO.getEngineInstance(instanceConfig);
+      window.engine = this.engine;
+      window.TRAVISO = TRAVISO;
       this.pixiRoot.stage.addChild(this.engine);
-      
     },
-    startWorker() {
-      // farm
-      setTimeout(() => {
-        this.engine.moveCurrentControllableToLocation({ c: 5, r: 7 }, 0.25);
-      }, 1000);
-
-      setTimeout(() => {
-        this.engine.moveCurrentControllableToLocation({ c: 5, r: 6 }, 0.25);
-      }, 8000);
-
-      // water
-      setTimeout(() => {
-        this.engine.moveCurrentControllableToLocation({ c: 2, r: 6 }, 0.25);
-      }, 14000);
-
-      setTimeout(() => {
-        this.engine.moveCurrentControllableToLocation({ c: 5, r: 6 }, 0.25);
-      }, 34000);
-
-      // setTimeout(() => {
-      //   this.startWorker();
-      // }, 50000);
+    moveWorker(stage) {
+      if (stage < 2) {
+        this.workerFarm(stage);
+      } else {
+        this.workerWater(stage);
+      }
+    },
+    workerFarm(stage) {
+      if (stage === 0) {
+        setTimeout(() => {
+          this.engine.moveCurrentControllableToLocation(
+            { c: 3, r: 6 },
+            this.stepSpeed
+          );
+        }, 2000);
+      }
+      if (stage === 1) {
+        setTimeout(() => {
+          this.engine.moveCurrentControllableToLocation(
+            { c: 3, r: 5 },
+            this.stepSpeed
+          );
+        }, 3000);
+      }
+    },
+    workerWater(stage) {
+      if (stage === 2) {
+        setTimeout(() => {
+          this.engine.moveCurrentControllableToLocation(
+            { c: 1, r: 5 },
+            this.stepSpeed
+          );
+        }, 2000);
+      }
+      if (stage === 3) {
+        setTimeout(() => {
+          this.engine.moveCurrentControllableToLocation(
+            { c: 3, r: 5 },
+            this.stepSpeed
+          );
+        }, 3000);
+      }
     },
     resizePIXE() {
       if (!this.isRenderFarm) {
@@ -122,6 +218,56 @@ export default {
         document.querySelector(".v-main__wrap").clientHeight
       );
     },
+    openModalWorker() {
+      this.modalWorker = true;
+    },
+    startWorker() {
+      this.isLoadingWorker = true;
+      setTimeout(() => {
+        this.modalWorker = false;
+        this.isProgressWorker = true;
+        this.isLoopWork = true;
+        this.moveWorker(this.stageWork);
+        this.percetageProcess();
+      }, 3000);
+    },
+    percetageProcess() {
+      if (this.percetageProgress >= 100) {
+        this.isProgressWorker = false;
+        this.isClaim = true;
+        this.isLoopWork = false;
+      } else {
+        this.percetageProgress += 10;
+        setTimeout(() => {
+          this.percetageProcess();
+        }, 2000);
+      }
+    },
+    claim() {
+      this.isClaim = false;
+    }
   },
 };
 </script>
+
+<style scoped>
+.menuWorker {
+  position: absolute;
+  width: 100%;
+  margin: 10px auto;
+}
+.progressWorker {
+  width: 300px;
+  margin: 0px auto;
+}
+.progressWorker > img {
+  z-index: 2;
+  margin-right: -2px;
+}
+.progressWorker >>> .v-progress-linear {
+  border: 2px solid #bb7248;
+  border-radius: 3px;
+  width: 100% !important;
+  background: #3a2720;
+}
+</style>
