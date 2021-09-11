c0652c8e16fc7e321962b25c0405f0575e1ca936
orc-tzan <>
2021-09-10 21:30:12 -0300

    war#3

--- src/data/Wars.js
+++ src/data/Wars.js
@@ -270,7 +270,53 @@ const wars = [
     test: true,
     showTest: true,
     showReedemPrize: true,
-  },  
+  }, 
+  {
+    id: 13,
+    name: "The third conflict [5]",
+    description: `
+    For a long time all the attention in Decentraland was spent on the rising tensions between Humans and Orcs, after a golden period of bonanza an age of military preparation has begun. A lot of wGOLD was spent in recruiting Warriors and Archers across the Human empire, from the Capital to the extended countryside, men of all ages were being recruited, children and seniors capable of holding a sword were considered able to fight. At the same time at Orc pits, Grunts and Orc Archers were being raised and united to form a monstrous army. This animosity and arousement of forces shadowed the real threat to liberty of all races that was lurking into the shadows. Silently in an obscure form the Forces of Extreme Darkness were growing in power day by day, night by night in a subversive manner. This ultimate enemy has not yet been faced.`,
+    dateStart: "2021-09-12",
+    dateEnd: "2021-09-17",
+    contractAddress: {
+      "97": "0x4544C8efb214830027306D68B29f0B5E37501D78",
+    },
+    countdown: {
+      enlistment: 1626361220000,
+      round1: 1626429080000,
+      round2: 1626443480000,
+    },
+    totalPrize: "2000000000000000000000000",
+    test: true,
+    showTest: true,
+    showReedemPrize: true,
+  },
+  {
+    id: 14,
+    name: "The third conflict",
+    description: `
+    Tales of sages describe the first war as thrilling, their old tomes comprise a story about a miraculous twist in the war that made the Corporation thrive even is disadvantageous circumstances. The Degenerate were brought home discouraged and fearful. But a foul scourge of cadaveric beings raised from the corpses of the fallen to join forces with the Degenerates. Known as the undead, this race of skeletons, witches and warlocks uses dark magic as vital force and means of destruction towards its enemies.
+    Both factions are preparing for an eminent conflict of unseen proportions, while some pray for protection, others seek vengeance.`,
+    dateStart: "2021-09-12",
+    dateEnd: "2021-09-17",
+    contractAddress: {
+      "5777": "",
+      "56": "0x92B2898267eDc65e8915e7c47ab2BeaDB19dc681",
+      "97": "",
+    },
+    countdown: {
+      // enlistment: 1631491200000,
+      enlistment: 0,
+      round1: 1631751480000,
+      round2: 1631837880000,
+    },
+    totalPrize: "800000000000000000000000",
+    test: false,
+    showTest: false,
+    showReedemPrize: true,
+    reportVersion: "2",
+    report: war2FinalReport,
+  },
 ];
 
 export function getWars(isTest) {
