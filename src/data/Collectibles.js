90189fef7694dd5761e84ac56e0776d5befe0652
dmenethil <dmenethil@protonmail.com>
2021-09-13 12:23:34 -0300

    Library dynamic tabs

--- src/data/Collectibles.js
+++ src/data/Collectibles.js
@@ -2,6 +2,7 @@ import store from "@/store";
 import { getGameItems } from "./Collectibles/GameItems";
 import { getLegendaryRelics } from "./Collectibles/LegendaryRelics";
 import { getWeapons } from "./Collectibles/Weapons";
+import { getMagicalItems } from "./Collectibles/MagicalItems";
 
 export function getCollectibles() {
   const addresses = store.getters["user/addresses"];
@@ -10,5 +11,5 @@ export function getCollectibles() {
     return [];
   }
 
-  return [].concat(getGameItems(), getLegendaryRelics(), getWeapons());
+  return [].concat(getGameItems(), getLegendaryRelics(), getWeapons(), getMagicalItems());
 }
