88d282e4dd804fd3d141ed903516804e61e8439a
dmenethil <dmenethil@protonmail.com>
2021-09-27 14:17:37 -0300

    Add Foundations data structure base

---
+++ src/data/Foundations.js
@@ -0,0 +1,49 @@
+export function getFoundations() {
+  return [
+    {
+      id: 1,
+      type: "foundations",
+      typeDesc: "Foundations",
+      image: '/images/foundations/1.png',
+      name: "village",
+      title: 'Village',
+      description: "Manage a village.",
+    },
+    {
+      id: 2,
+      type: "foundations",
+      typeDesc: "Foundations",
+      image: '/images/foundations/2.png',
+      name: "hideout",
+      title: 'Hideout',
+      description: "Store resources to avoid looting.<br /><small><i>% of stocked resources defined by owner</i></small>",
+    },
+    {
+      id: 3,
+      type: "foundations",
+      typeDesc: "Foundations",
+      image: '/images/foundations/3.png',
+      name: "temple",
+      title: 'Temple',
+      description: "Increase courage (15%)<br /><small><i>membership fee defined by owner</i></small>",
+    },
+    {
+      id: 4,
+      type: "foundations",
+      typeDesc: "Foundations",
+      image: '/images/foundations/4.png',
+      name: "watch-tower",
+      title: 'Watch Tower',
+      description: "Notification of attacks and troop movements.<br /><small><i>membership fee defined by owner</i></small>",
+    },
+    {
+      id: 5,
+      type: "foundations",
+      typeDesc: "Foundations",
+      image: '/images/foundations/5.png',
+      name: "market",
+      title: 'Market',
+      description: "Facilitate exchange between players.<br /><small><i>% of trades defined by owner</i></small>",
+    },
+  ];
+}
