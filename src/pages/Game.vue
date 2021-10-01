813768c4a6cf25c84363e9c98c9ea06fa706a882
orc-tzan <>
2021-10-01 19:21:20 -0300

    buy wLAND in the world

---
+++ src/pages/Game.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <iframe
+      id="gameArcadia"
+      width="100%"
+      src="/Game/Arcadia/?worldManager=0x9F4B5A875eA98f699E8B6Af93eb5B1d2Db508609"
+      frameborder="0"
+      scrolling="no"
+    ></iframe>
+  </div>
+</template>
+
+<script>
+export default {
+  components: {},
+
+  data() {
+    return {};
+  },
+
+  mounted() {
+    this.$nextTick(() => {
+      this.resizeIframe();
+    });
+    window.addEventListener("resize", this.resizeIframe);
+    document.getElementsByTagName("html")[0].style.overflow = 'hidden';
+  },
+
+  methods: {
+    resizeIframe() {
+      const footer = document.querySelector(".v-footer").clientHeight;
+      const appBar = document.querySelector(".v-app-bar").clientHeight;
+      const reduceHeight = footer + appBar;
+      document.querySelector("#gameArcadia").height = document.getElementsByTagName("html")[0].clientHeight - reduceHeight;
+    },
+  },
+};
+</script>
