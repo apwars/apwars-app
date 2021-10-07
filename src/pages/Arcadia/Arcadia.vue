ad55d6aa8c5edc40789e16ed364d8228ac595298
orc-tzan <>
2021-10-07 17:36:09 -0300

    game arcadia version 1.0.0.11

--- src/pages/Arcadia/Arcadia.vue
+++ src/pages/Arcadia/Arcadia.vue
@@ -1,9 +1,12 @@
 <template>
   <div>
     <iframe
+      v-if="isConnected"
       id="gameArcadia"
       width="100%"
-      src="/Game/Arcadia/?worldManager=0xD55eDCe573d50593088c0b9f7263E0870975089a&expectedVersion=1.0.0.10"
+      :src="
+        `/Game/Arcadia/?worldManager=${getWorldManager}&expectedVersion=1.0.0.11`
+      "
       frameborder="0"
       scrolling="no"
     ></iframe>
@@ -15,27 +18,58 @@ export default {
   components: {},
 
   data() {
-    return {};
+    return {
+      worldManager: {
+        "56": "",
+        "97": "0xDBe11E0663A23e2444e12Ee8677f72039b21890A",
+      },
+    };
+  },
+
+  computed: {
+    isConnected() {
+      return this.$store.getters["user/isConnected"];
+    },
+    networkInfo() {
+      return this.$store.getters["user/networkInfo"];
+    },
+    getWorldManager() {
+      return this.worldManager[this.networkInfo.id];
+    },
+  },
+
+  watch: {
+    isConnected() {
+      this.initData();
+    },
   },
 
   mounted() {
-    this.$nextTick(() => {
-      this.resizeIframe();
-    });
-    window.addEventListener("resize", this.resizeIframe);
-    document.getElementsByTagName("html")[0].style.overflow = 'hidden';
+    if (!this.isConnected) {
+      return;
+    }
+    this.initData();
   },
 
   beforeDestroy() {
-    document.getElementsByTagName("html")[0].style.overflow = 'auto';
+    document.getElementsByTagName("html")[0].style.overflow = "auto";
   },
 
   methods: {
+    initData() {
+      this.$nextTick(() => {
+        this.resizeIframe();
+      });
+      window.addEventListener("resize", this.resizeIframe);
+      document.getElementsByTagName("html")[0].style.overflow = "hidden";
+    },
+
     resizeIframe() {
       const footer = document.querySelector(".v-footer").clientHeight;
       const appBar = document.querySelector(".v-app-bar").clientHeight;
       const reduceHeight = footer + appBar;
-      document.querySelector("#gameArcadia").height = document.getElementsByTagName("html")[0].clientHeight - reduceHeight;
+      document.querySelector("#gameArcadia").height =
+        document.getElementsByTagName("html")[0].clientHeight - reduceHeight;
     },
   },
 };
