df5bcc407a5acf54a26d940072726df3db2687e0
dmenethil <dmenethil@protonmail.com>
2021-10-08 16:37:08 -0300

    Adjust addresses

--- src/data/ContractFactory.js
+++ src/data/ContractFactory.js
@@ -3,27 +3,33 @@ import wCOURAGE from '@/lib/eth/wCOURAGE';
 
 import { getCollectibleById } from "./Collectibles";
 
+const COLLECTIBLES_CONTRACTS = ['0x79ab3a6f3f1627535a8774fd2feed322d58f2d02', '0x627Ee66670c897B3121954d44018A79f1FdC3320'];
+
 export default (address) => {
 
     return {
       getContract: () => {
         switch (address) {
+            case '0x7ab6eedd9443e0e98e5ea93667ca5b250b8dda51':
             case '0x878a44a634f2F1aa925f2A0231fdBdb5F47CF08b':
                 return new wCOURAGE(address);
+            case '0x5F51A3ce7f2233777328866F477E86a91CA9DdeC':
             case '0x3A5c025065a14EF9e834fBD90aeD3876a07c60EA':
                 return new wGOLD(address);
         }
       },
       getName: () => {
         switch (address) {
+            case '0x7ab6eedd9443e0e98e5ea93667ca5b250b8dda51':
             case '0x878a44a634f2F1aa925f2A0231fdBdb5F47CF08b':
                 return 'wCOURAGE';
-            case '0x3A5c025065a14EF9e834fBD90aeD3876a07c60EA':
+                case '0x5F51A3ce7f2233777328866F477E86a91CA9DdeC':
+                  case '0x3A5c025065a14EF9e834fBD90aeD3876a07c60EA':
                 return 'wGOLD';              
         }
       },
       getCollectibleById(id) {
-        if (address !== '0x3fdE3A5FbC76b4AaB5955ed091DfcE2f84fA3Fb0') {
+        if (COLLECTIBLES_CONTRACTS.includes(address)) {
           return null;
         }
         return getCollectibleById(id) 
--- src/lib/components/ui/Combinators/GameItemsCombinators.vue
+++ src/lib/components/ui/Combinators/GameItemsCombinators.vue
@@ -1,167 +1,182 @@
 <template>
-  <v-container     
-    :fluid="$vuetify.breakpoint.md || $vuetify.breakpoint.mobile">
-    <div>
-      <div
-        cols="12"
-        lg="6"
-        xl="4"
-        class="d-flex justify-center">
-        <div class="align-self-center">  
+  <div>
+    <div
+      class="d-flex justify-center"
+      v-bind:class="{ disabled: gameItems.disabled }"
+    >
+      <div class="align-self-center">
+        <v-img
+          :width="$vuetify.breakpoint.mobile ? 130 : 165"
+          :src="gameItems.image"
+        />
+      </div>
+      <div v-if="isLoadingUnit" class="ml-1 align-self-start">
+        <div class="title">Necessary Resources</div>
+        <div class="d-flex qty">
           <v-img
-            :width="$vuetify.breakpoint.mobile ? 100 : 160"
-            :src="gameItems.image"
+            class="mr-1"
+            max-width="30px"
+            :src="
+              gameItems.combinators.warPreparation.necessaryResources.tokenA
+                .image
+            "
+            style="margin-left: -2px;"
+          />
+          <amount
+            :amount="getTokenAConfig.amount"
+            decimals="2"
+            :symbol="tokenAName"
           />
         </div>
-        <div v-if="isLoadingUnit" class="ml-1 align-self-start">
-          <div class="title">Necessary Resources</div>
-          <div class="d-flex qty">
-            <v-img class="mr-1" max-width="30px" :src="gameItems.combinators.warPreparation.necessaryResources.tokenA.image" 
-              style="margin-left: -2px;"/>
-            <amount
-              :amount="getTokenAConfig.amount"
-              decimals="2"
-              :symbol="tokenAName"
-            />
-          </div>
-          <div class="d-flex mt-1 qty">
-            <v-img
-              class="mr-1"
-              max-width="26px"
-              height="40px"
-              :src="gameItems.combinators.warPreparation.necessaryResources.tokenB.image"
-            />
-            <amount
-              :amount="getGameItemBConfig.amount"
-              decimals="0"
-              formatted
-              :symbol="gameItems.combinators.warPreparation.necessaryResources.tokenB.title"
-            />
-          </div>
-          <div class="d-flex align-center my-1 qty">
-            <img class="mr-1" max-width="26px" height="35px" src="/images/icons/hourglass.png"
-              style="margin-left: -4px;"/>
+        <div class="d-flex mt-1 qty">
+          <v-img
+            class="mr-1"
+            max-width="26px"
+            height="40px"
+            :src="
+              gameItems.combinators.warPreparation.necessaryResources.tokenB
+                .image
+            "
+          />
+          <amount
+            :amount="getGameItemBConfig.amount"
+            decimals="0"
+            formatted
+            :symbol="
+              gameItems.combinators.warPreparation.necessaryResources.tokenB
+                .title
+            "
+          />
+        </div>
+        <div class="d-flex align-center my-1 qty">
+          <img
+            class="mr-1"
+            max-width="26px"
+            height="35px"
+            src="/images/icons/hourglass.png"
+            style="margin-left: -4px;"
+          />
 
-            <div class="d-flex flex-column">
-              <span class="d-flex align-items-center justify-content-justify">
-                <span>
-                  Working time:
-                  <amount
-                    :amount="getGeneralConfig.blocks"
-                    decimals="0"
-                    formatted
-                  />
-                  blocks
-                </span>
+          <div class="d-flex flex-column">
+            <span class="d-flex align-items-center justify-content-justify">
+              <span>
+                Working time:
+                <amount
+                  :amount="getGeneralConfig.blocks"
+                  decimals="0"
+                  formatted
+                />
+                blocks
               </span>
-              <span><time-block :blocks="getGeneralConfig.blocks"/></span>
-            </div>
+            </span>
+            <span><time-block :blocks="getGeneralConfig.blocks"/></span>
           </div>
-          <hr />
-          <div class="d-flex mt-1 qty" v-if="gameItems.image">
-            <img
-              class="mr-1"
-              max-width="30px"
-              height="40px"
-              :src="gameItems.combinators.warPreparation.image"
-            />
-            <span style="margin-top: -5px">
-              Item conquered: <br />
-              <amount
+        </div>
+        <hr />
+        <div class="d-flex mt-1 qty" v-if="gameItems.image">
+          <img
+            class="mr-1"
+            max-width="30px"
+            height="40px"
+            :src="gameItems.combinators.warPreparation.image"
+          />
+          <span style="margin-top: -5px">
+            Item conquered: <br />
+            <amount
               :amount="getGameItemCConfig.amount"
               formatted
               decimals="0"
             />
-              {{ gameItems.title }}
-            </span>
-          </div>
-          <wButton
-            v-if="!isApproved"
-            class="mt-1"
-            :disabled="!getGeneralConfig.isEnabled"
-            @click="openModalArimedesApproval()"
-          >
-            Approve Research
-          </wButton>
-          <wButton
-            v-else-if="combinators.combinatorId === '0'"
-            :disabled="!getGeneralConfig.isEnabled"
-            @click="openModalArimedesNewResearch"
-            class="mt-1"
-          >
-            New Research
-          </wButton>
-          <wButton
-            v-else-if="combinators.combinatorId !== '0' && isClaim"
-            @click="openModalClaim()"
-            class="mt-1"
-          >
-            Claim
-          </wButton>
-          <div class="mt-1" v-else>
-            <span class="qty">Time for claim:</span>
-            <countdown-block
-              v-if="getGeneralConfig.isEnabled"
-              :start-blocks="combinators.startBlock"
-              :end-blocks="combinators.endBlock"
-              show-progress
-              @end="loadCombinators()"
-            />
-          </div>
+            {{ gameItems.title }}
+          </span>
         </div>
-        <v-skeleton-loader
-          v-else
-          class="mt-3 mx-1"
-          width="200px"
-          type="text, paragraph, paragraph, paragraph, button"
-        ></v-skeleton-loader>
-      </div> 
-      
-      <arimedes-modal
-        v-if="modalArimedesApproval"
-        :open="modalArimedesApproval"
-        @confirm="approveContract"
-        @close="modalArimedesApproval = false"
-        :isLoading="isLoadingApprove"
-        :text="textArimedesModal"
-        :textConfirm="textConfirmArimdesModal">
-      </arimedes-modal>
-
-      <arimedes-modal
-        v-if="modalArimedesClaim"
-        :open="modalArimedesClaim"
-        @confirm="claim"
-        @close="modalArimedesClaim = false"
-        :isLoading="isLoadingClaim"
-        :text="textClaim"
-        :textConfirm="textConfirmClaim"
-        :weaponIcon="gameItems.image"
-      ></arimedes-modal>
-
-      <arimedes-modal
-        v-if="modalArimedesNoBalance"
-        :open="modalArimedesNoBalance"
-        @close="modalArimedesNoBalance = false"
-        :text="textNoBalance"
-        :weaponIcon="gameItems.image"
-        hideConfirm
-      ></arimedes-modal>
-
-      <combinator-modal
-        v-if="modalArimedesNewResearch"
-        :open="modalArimedesNewResearch"
-        @confirm="combineTokens"
-        @close="modalArimedesNewResearch = false"
-        :isLoading="isLoadingNewResearch"
-        :info="combinatorInfo"
-        textConfirm="Research"
-        title="War Preparation"
-        :dontFormat="true"
-        width="800px"
-        height="360px"
-      ></combinator-modal>
+        <wButton
+          v-if="!isApproved"
+          class="mt-1"
+          :disabled="!getGeneralConfig.isEnabled"
+          @click="openModalArimedesApproval()"
+        >
+          Approve Research
+        </wButton>
+        <wButton
+          v-else-if="combinators.combinatorId === '0'"
+          :disabled="!getGeneralConfig.isEnabled"
+          @click="openModalArimedesNewResearch"
+          class="mt-1"
+        >
+          New Research
+        </wButton>
+        <wButton
+          v-else-if="combinators.combinatorId !== '0' && isClaim"
+          @click="openModalClaim()"
+          class="mt-1"
+        >
+          Claim
+        </wButton>
+        <div class="mt-1" v-else>
+          <span class="qty">Time for claim:</span>
+          <countdown-block
+            v-if="getGeneralConfig.isEnabled"
+            :start-blocks="combinators.startBlock"
+            :end-blocks="combinators.endBlock"
+            show-progress
+            @end="loadCombinators()"
+          />
+        </div>
+      </div>
+      <v-skeleton-loader
+        v-else
+        class="mt-3 mx-1"
+        width="200px"
+        type="text, paragraph, paragraph, paragraph, button"
+      ></v-skeleton-loader>
     </div>
-  </v-container>
+
+    <arimedes-modal
+      v-if="modalArimedesApproval"
+      :open="modalArimedesApproval"
+      @confirm="approveContract"
+      @close="modalArimedesApproval = false"
+      :isLoading="isLoadingApprove"
+      :text="textArimedesModal"
+      :textConfirm="textConfirmArimdesModal"
+    >
+    </arimedes-modal>
+
+    <arimedes-modal
+      v-if="modalArimedesClaim"
+      :open="modalArimedesClaim"
+      @confirm="claim"
+      @close="modalArimedesClaim = false"
+      :isLoading="isLoadingClaim"
+      :text="textClaim"
+      :textConfirm="textConfirmClaim"
+      :weaponIcon="gameItems.image"
+    ></arimedes-modal>
+
+    <arimedes-modal
+      v-if="modalArimedesNoBalance"
+      :open="modalArimedesNoBalance"
+      @close="modalArimedesNoBalance = false"
+      :text="textNoBalance"
+      :weaponIcon="gameItems.image"
+      hideConfirm
+    ></arimedes-modal>
+
+    <combinator-modal
+      v-if="modalArimedesNewResearch"
+      :open="modalArimedesNewResearch"
+      @confirm="combineTokens"
+      @close="modalArimedesNewResearch = false"
+      :isLoading="isLoadingNewResearch"
+      :info="combinatorInfo"
+      textConfirm="Research"
+      title="War Preparation"
+      :dontFormat="true"
+      width="800px"
+      height="360px"
+    ></combinator-modal>
+  </div>
 </template>
 
 <script>
@@ -175,20 +190,15 @@ import CombinatorModal from "@/lib/components/ui/Modals/CombinatorModal";
 import ToastSnackbar from "@/plugins/ToastSnackbar";
 import Convert from "@/lib/helpers/Convert";
 
-
 import Combinator from "@/lib/eth/Combinator";
-import wCOURAGE from "@/lib/eth/wCOURAGE";
 import Collectibles from "@/lib/eth/Collectibles";
 import ContractFactory from "@/data/ContractFactory";
 
-const ARIMEDES_APPROVE_SECOND_PAGE_CONTRACT =
-  "And now sign over here..";
+const ARIMEDES_APPROVE_SECOND_PAGE_CONTRACT = "And now sign over here..";
 const ARIMEDES_APPROVE_FIRST_PAGE_CONTRACT =
   "I will need two signatures from you to begin the research... This is the first";
-const ARIMEDES_APPROVE_ONLY_ONE_PAGE_CONTRACT =
-  "Please sign over here...";
-const ARIMEDES_WAITING_WALLET_APPROVAL =
-  "Please sign over here...";
+const ARIMEDES_APPROVE_ONLY_ONE_PAGE_CONTRACT = "Please sign over here...";
+const ARIMEDES_WAITING_WALLET_APPROVAL = "Please sign over here...";
 const ARIMEDES_WAITING_FIRST_CONFIRMATION =
   "I am checking if everything is fine..";
 const ARIMEDES_WAITING_SECOND_CONFIRMATION =
@@ -293,7 +303,9 @@ export default {
     },
 
     combinatorAddress() {
-      return this.gameItems.combinators.warPreparation.combinatorAddress[this.networkInfo.id];
+      return this.gameItems.combinators.warPreparation.combinatorAddress[
+        this.networkInfo.id
+      ];
     },
 
     combinatorId() {
@@ -308,7 +320,10 @@ export default {
     },
 
     infoTraining() {
-      if (this.gameItems.combinators && this.gameItems.combinators.warPreparation) {
+      if (
+        this.gameItems.combinators &&
+        this.gameItems.combinators.warPreparation
+      ) {
         return this.gameItems.combinators.warPreparation;
       }
       return {};
@@ -323,7 +338,10 @@ export default {
       const amountGameItemB = this.getGameItemBConfig.amount;
       const balanceGameItemB = this.getGameItemBConfig.balance;
 
-      if (balanceTokenA >= amountTokenA && balanceGameItemB >= amountGameItemB) {
+      if (
+        balanceTokenA >= amountTokenA &&
+        balanceGameItemB >= amountGameItemB
+      ) {
         return true;
       }
 
@@ -361,38 +379,40 @@ export default {
         this.combinatorId
       );
       this.tokenA = tokenAConfig.tokenAddress;
-      this.GameItemB = this.addresses.collectibles
+      this.GameItemB = this.addresses.collectibles;
       this.tokenAContract = ContractFactory(this.tokenA).getContract();
       this.tokenAName = ContractFactory(tokenAConfig.tokenAddress).getName();
       this.GameItemBContract = new Collectibles(this.GameItemB);
     },
-    
+
     async loadData() {
       if (!this.isConnected) {
         return;
       }
 
-      if (this.getGeneralConfig.isEnabled !== undefined &&
-        !this.getGeneralConfig.isEnabled) {
+      if (
+        this.getGeneralConfig.isEnabled !== undefined &&
+        !this.getGeneralConfig.isEnabled
+      ) {
         return;
       }
-      
+
       this.isApprovedTokenA = await this.tokenAContract.hasAllowance(
         this.account,
         this.combinatorAddress
       );
-      
+
       this.isApprovedGameItemB = await this.GameItemBContract.isApprovedForAll(
         this.account,
         this.combinatorAddress
       );
-      
+
       this.getGeneralConfig = await this.combinatorContract.getGeneralConfig(
         this.account,
         this.account,
         this.combinatorId
       );
-      
+
       if (this.getGeneralConfig.isEnabled) {
         this.getTokenAConfig = {
           ...this.getTokenAConfig,
@@ -504,7 +524,7 @@ export default {
         this.textArimedesModal = ARIMEDES_WAITING_WALLET_APPROVAL;
         const confirmTransaction = this.approveOnlyOneContract.approve(
           this.account,
-         this.combinatorAddress
+          this.combinatorAddress
         );
 
         confirmTransaction.on("error", (error) => {
@@ -550,7 +570,7 @@ export default {
         this.textArimedesModal = ARIMEDES_WAITING_WALLET_APPROVAL;
         const confirmTransaction = this.tokenAContract.approve(
           this.account,
-         this.combinatorAddress
+          this.combinatorAddress
         );
 
         confirmTransaction.on("error", (error) => {
@@ -616,26 +636,34 @@ export default {
         const amountTokenA = Convert.fromWei(this.getTokenAConfig.amount, true);
         const amountGameItemB = this.getGameItemBConfig.amount;
 
-        const GameItemBName = this.gameItems.combinators.warPreparation.necessaryResources.tokenB.title;
+        const GameItemBName = this.gameItems.combinators.warPreparation
+          .necessaryResources.tokenB.title;
 
         this.textNoBalance = `I see that you do not have the necessary requirements for research. You must have ${amountTokenA} wCOURAGE and ${amountGameItemB} ${GameItemBName}.`;
         return (this.modalArimedesNoBalance = true);
       }
       this.modalArimedesNewResearch = true;
 
-
       const { collectibles, id } = this.getGameItemBConfig;
-      const tokenBName = ContractFactory(collectibles).getCollectibleById(parseInt(id)).name;
+      const tokenBName = ContractFactory(collectibles).getCollectibleById(
+        parseInt(id)
+      ).name;
 
       this.combinatorInfo = {
         ...this.combinatorInfo,
         ...{
           getGeneralConfig: this.getGeneralConfig,
           getTokenAConfig: { ...this.getTokenAConfig, name: this.tokenAName },
-          getGameItemBConfig: { ...this.gameItems.combinators.warPreparation.necessaryResources.tokenB.image, ...this.getGameItemBConfig, name: tokenBName },
+          getGameItemBConfig: {
+            ...this.gameItems.combinators.warPreparation.necessaryResources
+              .tokenB.image,
+            ...this.getGameItemBConfig,
+            name: tokenBName,
+          },
           getTokenCConfig: this.getGameItemCConfig,
           infoTraining: this.infoTraining,
-          combinatorData: this.gameItems.combinators.warPreparation.necessaryResources
+          combinatorData: this.gameItems.combinators.warPreparation
+            .necessaryResources,
         },
       };
 
@@ -710,7 +738,7 @@ export default {
         return ToastSnackbar.error(error);
       }
     },
-  }
+  },
 };
 </script>
 
