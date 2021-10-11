fce3ad4e189ea75edcc44a07c6d292e4056f5324
orc-tzan <>
2021-10-11 11:12:59 -0300

    removing metamask modal

--- src/lib/components/ui/App/AppFooter.vue
+++ src/lib/components/ui/App/AppFooter.vue
@@ -8,7 +8,6 @@
       >
         <div class="d-inline-flex">
           <img
-            v-if="account"
             class="d-block mx-1 mx-md-2 avatar"
             :src="
               `https://avatar.apwars.farm/?seed=${account}&avatar=${avatar}`
@@ -29,6 +28,9 @@
               decimals="2"
               symbol="wGOLD"
             />
+            <span v-else>
+              0.00 wGOLD
+            </span>
           </span>
         </div>
       </div>
