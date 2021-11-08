f067ee82f8f1cc004e01f5da63344a9a9755c543
luthien-elve <luthien-elve@protonmail.com>
2021-11-08 19:26:23 -0300

    Added weapons filters on war preparations page, on weapons tab

--- src/lib/components/ui/Lists/ListCombinators.vue
+++ src/lib/components/ui/Lists/ListCombinators.vue
@@ -4,17 +4,95 @@
       :fluid="$vuetify.breakpoint.md || $vuetify.breakpoint.mobile"
       v-if="isConnected && !isLoading"
     >
-      <v-row v-if="tokens.length > 0">
+      <v-row :no-gutters="$vuetify.breakpoint.mobile">
+        <v-col cols="12" lg="3">
+          <v-select
+            v-model="select.teams"
+            :items="filter.teamDesc"
+            label="Select Factions"
+            multiple
+            chips
+            solo
+            @change="updateTroopsFilters()"
+          >
+          </v-select>
+        </v-col>
+        <v-col cols="12" lg="3">
+          <v-select
+            v-model="select.tiers"
+            :items="filter.tierDesc"
+            label="Select Tier"
+            multiple
+            chips
+            solo
+            @change="updateTroopsFilters()"
+          >
+          </v-select>
+        </v-col>
+        <v-col cols="12" lg="3">
+          <v-select
+            v-model="select.races"
+            :items="filter.raceDesc"
+            label="Select Races"
+            multiple
+            chips
+            solo
+            @change="updateTroopsFilters()"
+          >
+          </v-select>
+        </v-col>
+        <v-col cols="12" lg="3">
+          <v-select
+            v-model="select.names"
+            :items="filter.name"
+            label="Select Units"
+            multiple
+            chips
+            solo
+            @change="updateTroopsFilters()"
+          >
+            <template v-slot:selection="{ item, index }">
+              <v-chip v-if="index === 0">
+                <span>{{ item }}</span>
+              </v-chip>
+              <span v-if="index === 1" class="grey--text text-caption">
+                (+{{ select.names.length - 1 }} others)
+              </span>
+            </template>
+          </v-select>
+        </v-col>
+      </v-row>
+      <v-row class="mt-0 mt-lg-n5 mb-3">
+        <v-col class="py-0 my-0" cols="12">
+          <div class="d-flex flex-column flex-md-row align-center">
+            <wButton @click="clearFilters()" class=" mr-3">
+              <div class="d-flex justify-center">
+                <v-icon class="mx-1">
+                  mdi-minus-circle
+                </v-icon>
+                <small class="align-self-center">Clear filter</small>
+              </div>
+            </wButton>
+            <v-checkbox
+              v-model="showMyUnits"
+              @change="updateTroopsFilters()"
+              label="Show only my units"
+              color="primary"
+            ></v-checkbox>
+          </div>
+        </v-col>
+      </v-row>
+      <v-row v-if="filterTroops.length > 0">
         <v-col
           cols="12"
           lg="6"
           xl="4"
-          v-for="token in tokens"
-          v-bind:key="token.name"
+          v-for="trooper in filterTroops"
+          v-bind:key="trooper.name"
         >
-          <game-items-combinators :gameItems="token" v-if="type === 'magical-items'" />
+          <game-items-combinators :gameItems="trooper" v-if="type === 'magical-items'" />
           <unit-war-preparation v-else
-            :unit="token"
+            :unit="trooper"
           />
         </v-col>
       </v-row>
@@ -50,6 +128,9 @@ import UnitTrainingCenter from "@/lib/components/ui/Units/UnitTrainingCenter";
 
 import { getTroops } from "@/data/Troops";
 import { getMagicalItems } from "@/data/Collectibles/MagicalItems";
+import Troops from "@/lib/eth/Troops";
+import ToastSnackbar from "@/plugins/ToastSnackbar";
+import { getGameItems } from "@/data/Collectibles/GameItems";
 
 export default {
   components: {
@@ -80,7 +161,21 @@ export default {
       balance: 0,
       pricewGOLD: 0,
       isLoading: true,
-      tokens: [],
+      showMyUnits: false,
+      select: {
+        teams: [],
+        tiers: [],
+        races: [],
+        names: [],
+      },
+      filter: {
+        teamDesc: [],
+        raceDesc: [],
+        name: [],
+      },
+      teams: [],
+      globalTroops: [],
+      filterTroops: [],
     };
   },
 
@@ -125,23 +220,200 @@ export default {
   },
 
   methods: {
-    loadData() {
+    async loadData() {
       if (!this.isConnected) {
         return;
       }
 
-      let units = getTroops();
+      let units = await getTroops();
       if (this.type === 'magical-items') {
         units = getMagicalItems();
       }
 
       try {
-        this.tokens = units.filter(t => t.combinators?.warPreparation);
+        this.filterTroops = units.filter(t => t.combinators?.warPreparation);
         this.isLoading = false;
-      } catch (e) {
-        console.error(e)
-      }
+        this.globalTroops = await Promise.all(
+          units.map((trooper) => {
+            return new Promise(async (resolve) => {
+              try {
+                if (trooper.contractAddress === "") {
+                  resolve({
+                    name: trooper.name,
+                    team: trooper.team,
+                    tier: trooper.tier,
+                    myQty: "0",
+                    globalQty: "0",
+                    pricewGOLD: "0",
+                    disabled: true,
+                  });
+                }
+                const getTropper = new Troops(
+                  trooper.contractAddress[this.networkInfo.id]
+                );
+                const myQty = await getTropper.balanceOf(this.account);
+                const globalQty = await getTropper.totalSupply();
+                const pricewGOLD = await getTropper.pricewGOLD(
+                  trooper.lpAddresses,
+                  this.networkInfo.id
+                );
 
+                resolve({
+                  ...trooper,
+                  ...{
+                    myQty: myQty,
+                    globalQty: globalQty,
+                    pricewGOLD: pricewGOLD,
+                    disabled: false,
+                  },
+                });
+              } catch (error) {
+                resolve({
+                  name: trooper.name,
+                  team: trooper.team,
+                  tier: trooper.tier,
+                  myQty: "0",
+                  globalQty: "0",
+                  pricewGOLD: "0",
+                  disabled: true,
+                });
+              }
+            });
+          })
+        );
+
+        this.globalTroops = this.globalTroops.map((trooper) => {
+          return {
+            ...trooper,
+            ...{ globalQty: trooper.globalQty, myQty: trooper.myQty },
+          };
+        });
+
+        const gameItems = getGameItems().filter(g => g.combinators).map(g => ({...g, myQty: 0, teamDesc: '', raceDesc: '', tierDesc: '', name: g.title }));
+
+        this.globalTroops = this.globalTroops.concat(gameItems)
+        /* this.filterTroops = units.filter(t => t.combinators?.warPreparation); */
+        this.filterTroops = this.globalTroops.filter(t => t.combinators?.warPreparation);
+
+        //this.updateTroopsFilters();
+      } catch (error) {
+        if (error.message) {
+          return ToastSnackbar.error(error.message);
+        }
+        return ToastSnackbar.error(error);
+      } finally {
+        this.isLoading = false
+      }
+    },
+
+    updateSelectFilters() {
+      const filterSelect = {
+        raceDesc: [],
+        name: [],
+      };
+      const filterGlobal = {
+        teamDesc: [],
+        tierDesc: [],
+      };
+      const filterRaces = this.globalTroops.filter((trooper) => {
+        if (!this.select.teams.length) {
+          return trooper;
+        }
+        return this.select.teams.indexOf(trooper.teamDesc) !== -1;
+      });
+      filterRaces.map((trooper) => {
+        if (filterSelect.raceDesc.indexOf(trooper.raceDesc) === -1) {
+          filterSelect.raceDesc.push(trooper.raceDesc);
+        }
+      });
+      const filterNames = filterRaces.filter((trooper) => {
+        if (
+          this.select.tiers.length &&
+          this.select.tiers.indexOf(trooper.tierDesc) !== -1 &&
+          this.select.races.length &&
+          this.select.races.indexOf(trooper.raceDesc) !== -1
+        ) {
+          return trooper;
+        } else if (this.select.tiers.length && !this.select.races.length) {
+          return this.select.tiers.indexOf(trooper.tierDesc) !== -1;
+        } else if (!this.select.tiers.length && this.select.races.length) {
+          return this.select.races.indexOf(trooper.raceDesc) !== -1;
+        } else if (!this.select.tiers.length && !this.select.races.length) {
+          return trooper;
+        }
+      });
+      filterNames.map((trooper) => {
+        if (filterSelect.name.indexOf(trooper.name) === -1) {
+          filterSelect.name.push(trooper.name);
+        }
+      });
+      this.globalTroops.map((trooper) => {
+        for (const filter in filterGlobal) {
+          if (filterGlobal[filter].indexOf(trooper[filter]) === -1) {
+            filterGlobal[filter].push(trooper[filter]);
+          }
+        }
+      });
+
+      this.filter = { ...filterGlobal, ...filterSelect };
+
+      if (this.filter.teamDesc.length === 1) {
+        this.select.teams = [this.filter.teamDesc[0]];
+      }
+    },
+
+    async updateTroopsFilters() {
+      this.filterTroops = this.globalTroops.filter((trooper) => {
+        if (trooper.combinators?.warPreparation) { 
+          if (!this.select.teams.length) {
+            return trooper;
+          }
+          return this.select.teams.indexOf(trooper.teamDesc) !== -1;
+        }
+      });
+      this.filterTroops = this.filterTroops.filter((trooper) => {
+        if (trooper.combinators?.warPreparation) {
+          if (!this.select.tiers.length) {
+            return trooper;
+          }
+          return this.select.tiers.indexOf(trooper.tierDesc) !== -1;
+        }
+      });
+      this.filterTroops = this.filterTroops.filter((trooper) => {
+        if (trooper.combinators?.warPreparation) {
+          if (!this.select.races.length) {
+            return trooper;
+          }
+          return this.select.races.indexOf(trooper.raceDesc) !== -1;
+        }
+      });
+      this.filterTroops = this.filterTroops.filter((trooper) => {
+        if (trooper.combinators?.warPreparation) {
+          if (!this.select.names.length) {
+            return trooper;
+          }
+          return this.select.names.indexOf(trooper.name) !== -1;
+        }
+      });
+
+      if (this.showMyUnits) {
+        this.filterTroops = this.filterTroops.filter((trooper) => {
+          if (trooper.combinators?.warPreparation) {
+            return trooper.myQty != "0";
+          }
+        });
+      }
+      this.updateSelectFilters();
+    },
+
+    clearFilters() {
+      this.select = {
+        teams: [],
+        tiers: [],
+        races: [],
+        names: [],
+      };
+      this.updateTroopsFilters();
     },
   },
 };
