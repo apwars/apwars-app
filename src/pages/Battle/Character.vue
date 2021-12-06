3f13ca8384ff994d37cd01b37c8b7162c7f7bc2d
dmenethil <dmenethil@protonmail.com>
2021-12-06 15:00:56 -0300

    Base for char screen

---
+++ src/pages/Battle/Character.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="background">
+    <v-container>
+      <v-row>
+        <v-col></v-col>
+        <v-col></v-col>
+      </v-row>
+    </v-container>
+  </div>
+</template>
+<style lang="scss" scoped>
+.background {
+  height: 100%;
+  width: 100%;
+  background-image: url("/images/backgrounds/enlistment.png");
+}
+</style>
