8f48d5eac497bcecb0fc2ab565a487d96db4fc65
dmenethil <dmenethil@protonmail.com>
2021-12-06 17:36:04 -0300

    Add icons, Buttons, disable header

---
+++ src/lib/components/ui/Buttons/Button.vue
@@ -0,0 +1,132 @@
+<template>
+  <button
+    :class="['btn-container', [type], isBlock ? 'is-block' : '', noPadding ? 'no-padding' : '']"
+    @click="handleClick"
+    :disabled="disabled"
+  >
+    <img width="24" :src="`/images/icons/${icon}.png`" :alt="icon" v-if="icon" />
+    <game-text :class="['btn-tertiary-text', icon ? 'ml-1' : '']" v-if="type === 'wtertiary'">{{
+      text
+    }}</game-text>
+    <span v-else-if="text" :class="['btn-text', icon ? 'ml-2' : '']">{{ text }}</span>
+  </button>
+</template>
+<script>
+import GameText from "@/lib/components/ui/Utils/GameText";
+export default {
+  components: {
+    GameText,
+  },
+  props: {
+    text: {
+      type: String,
+      default: "",
+    },
+    type: String,
+    handleClick: {
+      type: Function,
+      default: () => {},
+    },
+    isBlock: {
+      type: Boolean,
+      default: false,
+    },
+    disabled: {
+      type: Boolean,
+      default: false,
+    },
+    noPadding: {
+      type: Boolean,
+      default: false,
+    },
+    icon: {
+      type: String,
+      default: '',
+    }
+  },
+};
+</script>
+<style lang="scss" scoped>
+.btn-container {
+  padding: 8px 16px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border: 1px solid white;
+  color: white;
+  text-align: center;
+  font-weight: bold;
+  font-size: 16px;
+  line-height: 21px;
+  height: 40px;
+  &.no-padding {
+    padding: 8px 0;
+  }
+  &:hover:enabled {
+    cursor: pointer;
+    background-color: #e5e5e5;
+    border: 1px solid #3a2720;
+  }
+  > .btn-text {
+    color: #3a2720;
+  }
+  &.is-block {
+    width: 100%;
+  }
+  &.wprimary {
+    background-color: #3a2720;
+    border: 1px solid #FFEEBC;
+    > .btn-text {
+      color: #fff;
+    }
+    &:hover:enabled {
+      background-color: #e5e5e5;
+      border: 1px solid #3a2720;
+      > .btn-text {
+        color: #3a2720;
+      }
+    }
+    &:focus:enabled{
+      filter: grayscale(100%);
+      > .btn-text {
+        color: #fff;
+      }
+      &:hover:enabled {
+        background-color: #3a2720;
+      }
+    }
+  }
+  &.wsecondary {
+    background-color: transparent !important;
+    border: 1px solid #FFF;;
+    > .btn-text {
+      color: white;
+    }
+    &:hover:enabled {
+      background-color: #e5e5e5!important;
+      border: 1px solid #3a2720;
+      > .btn-text {
+        color: #3a2720;
+      }
+    }
+  }
+  &.wtertiary {
+    border: none;
+    &:hover:enabled {
+      border: none;
+      background-color: transparent;
+      > .btn-tertiary-text {
+        cursor: pointer;
+        font-size: 17px;
+        transition: all 0.4s;
+      }
+    }
+  }
+  &:disabled {
+    background-color: #2A3238;
+    border: 1px solid white;
+    color: white;
+    cursor: not-allowed;
+  }
+}
+</style>
--- src/pages/Battle/Character.vue
+++ src/pages/Battle/Character.vue
@@ -2,10 +2,18 @@
   <div class="background">
     <v-container>
       <v-row>
-        <v-col>
+        <v-col class="d-flex">
+          <Button type="wprimary" text="Corps" class="mr-2" />
+          <Button type="wprimary" text="Degens" />
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col md="7">
           <v-row>
-            <v-col>
-              <div class="unit-image"></div>
+            <v-col class="d-flex">
+              <div class="unit-image">
+                <v-img src="/images/troops/wwarrior-toon.png" />
+              </div>
               <div class="unit-data">
                 <div class="input-name">Edit name</div>
                 <div class="class-info">Class: Warrior</div>
@@ -22,33 +30,72 @@
                     noText
                   />
                 </div>
-                <div class="items-container"></div>
+                <div class="items-container d-flex justify-space-between mt-2">
+                  <img src="/images/icons/slot.png" />
+                  <img src="/images/icons/slot.png" />
+                  <img src="/images/icons/slot.png" />
+                  <img src="/images/icons/slot.png" />
+                  <img src="/images/icons/slot.png" />
+                </div>
               </div>
             </v-col>
           </v-row>
           <v-row>
             <v-col class="d-flex"
               ><div class="text-large mr-2">Courage</div>
-              <ForceMeter :maxScale="100" :value="60" color="wyellow"
+              <ForceMeter :maxScale="100" :value="60" color="wgreen"
             /></v-col>
             <v-col class="d-flex"
               ><div class="text-large  mr-2">Energy</div>
-              <ForceMeter :maxScale="100" :value="10" color="wgreen" />
+              <ForceMeter :maxScale="100" :value="10" color="wyellow" />
             </v-col>
           </v-row>
         </v-col>
-        <v-col></v-col>
+        <v-col md="5">
+          <v-row>
+            <v-col md="4">
+              <div class="icon-container">
+                <v-img src="/images/icons/sword.png" />
+              </div>
+            </v-col>
+            <v-col md="4">
+              <div class="icon-container">
+                <v-img src="/images/icons/patent.png" />
+              </div>
+            </v-col>
+            <v-col md="4">
+              <div class="icon-container">
+                <v-img src="/images/icons/anvil.png" />
+              </div>
+            </v-col>
+          </v-row>
+        </v-col>
       </v-row>
     </v-container>
   </div>
 </template>
 <script>
+import { mapMutations } from "vuex";
+import Button from "@/lib/components/ui/Buttons/Button";
 import ForceMeter from "@/lib/components/ui/ForceMeter";
 import Progress from "@/lib/components/ui/Progress";
 export default {
   components: {
+    Button,
     ForceMeter,
     Progress,
+  },
+  methods: {
+    ...mapMutations({
+      setHeader: "app/setMenuDisplay",
+    }),
+  },
+  async mounted() {
+    this.setHeader(false);
+  },
+  beforeRouteLeave(to, from, next) {
+    this.setHeader(true);
+    next();
   },
 };
 </script>
@@ -64,13 +111,33 @@ export default {
   font-size: 18px;
   line-height: 24px;
 }
+.text-big {
+  font-weight: bold;
+  font-size: 22px;
+  line-height: 29px;
+}
 .text-large {
   font-weight: bold;
   font-size: 28px;
   line-height: 37px;
 }
+.unit-image {
+  max-width: 240px;
+  margin-right: 24px;
+}
+.unit-data {
+  width: 100%;
+}
+.class-info {
+  @extend .text-big;
+}
 .xp-label {
   @extend .text-medium;
   margin-bottom: 4px;
+}
+.icon-container {
+  width: 100%;
+  border-radius: 50%;
+  outline: 2px solid #FFEEBC;
 }
 </style>
