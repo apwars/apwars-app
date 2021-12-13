20f3cba33e7732f96d50db5066b76164f1bbb172
dmenethil <dmenethil@protonmail.com>
2021-12-13 10:47:09 -0300

    Base for API integration

---
+++ src/controller/SoldierController.js
@@ -0,0 +1,21 @@
+import BaseController from './BaseController';
+import store from "@/store";
+
+export default class SoldierController {
+  constructor() {
+    const addresses = store.getters["user/addresses"];
+    this.base = new BaseController(addresses.apiArcadia);
+  }
+
+  wallets(account) {
+    try {
+      return this.base._get(`/wallets/${account}`);
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  
+
+
+}
