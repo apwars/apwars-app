ccffbc4cc6dae2365dc8dccda425bc20ec09fe03
orc-tzan <>
2021-12-22 20:09:44 -0300

    1.1.0.11

--- src/pages/Arcadia/Arcadia.vue
+++ src/pages/Arcadia/Arcadia.vue
@@ -5,7 +5,7 @@
       id="gameArcadia"
       width="100%"
       :src="
-        `/Game/Arcadia/?worldManager=${getWorldManager}&landMap=${getLandMap}&treasureHunt=${getTreasureHunt}&treasureHuntId=${getTreasureHuntId}&expectedVersion=1.0.1.13`
+        `/Game/Arcadia/?worldManager=${getWorldManager}&landMap=${getLandMap}&treasureHunt=${getTreasureHunt}&treasureHuntId=${getTreasureHuntId}&expectedVersion=1.1.0.11`
       "
       frameborder="0"
       scrolling="no"
