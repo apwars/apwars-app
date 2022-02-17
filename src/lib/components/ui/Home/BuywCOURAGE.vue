946b31eb5156f9b21c9d419f9b0f92db10d3a793
orc-tzan <>
2022-02-17 12:32:14 -0300

    packwCOURAGE

--- src/lib/components/ui/Home/BuywCOURAGE.vue
+++ src/lib/components/ui/Home/BuywCOURAGE.vue
@@ -1,58 +1,74 @@
 <template>
-  <div class="courage-container">
-    <v-row no-gutters>
-      <v-col>
-        <div class="d-flex align-center justify-center">
-          <img class="mr-1" width="32px" height="28px" src="/images/icons/chest.png" alt="chest" />
-          <game-text header="h4" class="text-center">
-            wCOURAGE Packs
-          </game-text>
-        </div>
-      </v-col>
-    </v-row>
-    <v-row no-gutters>
-      <v-col cols="12" class="d-flex justify-center align-center">
-        <div class="d-flex justify-center align-center">
-          <div class="token-image">
-            <v-img src="/images/wcourage.png" alt="wCOURAGE" />
-            <div class="discount">
-              <div>20%</div>
-              <div>OFF</div>
-            </div>
+  <div>
+    <div v-if="!isLoading" class="courage-container">
+      <v-row no-gutters>
+        <v-col>
+          <div class="d-flex align-center justify-center">
+            <img
+              class="mr-1"
+              width="32px"
+              height="28px"
+              src="/images/icons/chest.png"
+              alt="chest"
+            />
+            <game-text header="h4" class="text-center">
+              wCOURAGE Packs
+            </game-text>
           </div>
+        </v-col>
+      </v-row>
+      <v-row no-gutters>
+        <v-col cols="12" class="d-flex justify-center align-center">
+          <div class="d-flex justify-center align-center">
+            <div class="token-image">
+              <v-img src="/images/wcourage.png" alt="wCOURAGE" />
+              <div class="discount">
+                <div>20%</div>
+                <div>OFF</div>
+              </div>
+            </div>
 
-          <div class="d-flex flex-column justify-center align-center">
-            <div class="qty">
-              5,000 wCOURAGE
-            </div>
-            <div class="price">
-              <span class="d-flex align-center"
-                >Price:
-                <span class="d-flex align-center ml-1"
-                  ><v-img
-                    src="/images/wgold.png"
-                    height="18px"
-                    width="18px"
-                    alt="wGOLD"
-                  />
-                  1000 wGOLD</span
-                ></span
-              >
-            </div>
-            <div>
-              <wButton
-                @click="buyPack"
-                class="mt-1"
-                size="small"
-                :disabled="isLoadingBuy"
-              >
-                Buy this pack
-              </wButton>
+            <div class="d-flex flex-column justify-center align-center">
+              <div class="qty">
+                <Amount :amount="packwCOURAGE.content[0].amount" formatted symbol="wCOURAGE" /> 
+              </div>
+              <div class="price">
+                <span class="d-flex align-center"
+                  >Price:
+                  <span class="d-flex align-center ml-1"
+                    ><v-img
+                      src="/images/wgold.png"
+                      height="18px"
+                      width="18px"
+                      alt="wGOLD"
+                    />
+                    <Amount :amount="packwCOURAGE.price.amount" formatted symbol="wGOLD" /> 
+                  </span></span>
+              </div>
+              <div>
+                <wButton
+                  @click="buyPack"
+                  class="mt-1"
+                  size="small"
+                  :disabled="isLoadingBuy || packwCOURAGE.remainingAmount <= 0"
+                >
+                  Buy this pack
+                </wButton>
+                <div class="qty-info">
+                  <template v-if="packwCOURAGE.remainingAmount > 0"
+                    >{{ packwCOURAGE.amount - packwCOURAGE.soldAmount }} /
+                    {{ packwCOURAGE.amount }}
+                  </template>
+                </div>
+              </div>
             </div>
           </div>
-        </div>
-      </v-col>
-    </v-row>
+        </v-col>
+      </v-row>
+    </div>
+    <div v-else>
+      Loading...
+    </div>
   </div>
 </template>
 <script>
@@ -62,15 +78,25 @@ import ToastSnackbar from "@/plugins/ToastSnackbar";
 
 import GameText from "@/lib/components/ui/Utils/GameText";
 import wButton from "@/lib/components/ui/Buttons/wButton";
+import Amount from "@/lib/components/ui/Utils/Amount";
 
 export default {
-  components: { GameText, wButton },
+  components: { GameText, wButton, Amount },
   data() {
     return {
+      packwCOURAGE: {},
+      isLoading: true,
       isLoadingBuy: false,
     };
   },
   methods: {
+    async getPackwCOURAGE() {
+      const controller = new PacksController();
+      let items = await controller.getByType("CELEBRATION_FESTIVAL");
+      this.packwCOURAGE = items.find(
+        (item) => item.package === "CELEBRATION_FESTIVAL_COURAGE"
+      );
+    },
     async buyPack() {
       try {
         const packageName = "CELEBRATION_FESTIVAL_COURAGE";
@@ -92,6 +118,10 @@ export default {
         this.isLoadingBuy = false;
       }
     },
+  },
+  async created() {
+    await this.getPackwCOURAGE();
+    this.isLoading = false;
   },
 };
 </script>
@@ -127,5 +157,17 @@ export default {
   justify-content: center;
   align-items: center;
   font-weight: bold;
+}
+
+.qty-info,
+.qty-balance {
+  height: 13px;
+  font-weight: bold;
+  font-size: 10px;
+  line-height: 13px;
+  text-align: center;
+  width: 100%;
+  color: #11d300;
+  margin-top: 2px;
 }
 </style>
