b985f40b6443f58785b6d7d64c87e01575afc6f4
dmenethil <dmenethil@protonmail.com>
2022-02-18 10:00:28 -0300

    Add profile screen

---
+++ src/pages/Account/Account.vue
@@ -0,0 +1,63 @@
+<template
+  ><div class="background">
+    <v-container>
+      <v-row>
+        <v-col><Title>Account</Title></v-col></v-row
+      >
+      <v-row>
+        <v-col> <v-row class="screen-container">
+          <v-col cols="12" md="4"><v-select v-model="faction" item-text="label" item-value="value" :items="factionOptions"/>
+          <v-text-field v-model="avatar" /></v-col>
+          <v-col cols="12" md="4">
+          <img
+            class="avatar"
+            :src="
+              `https://avatar.apwars.farm/?seed=${avatar || account}&avatar=${faction}`
+            "
+            alt="avatar"
+          />
+          </v-col></v-row>
+        </v-col>
+      </v-row>
+    </v-container></div
+></template>
+<script>
+import Title from "@/lib/components/ui/Title";
+
+export default {
+  components: { Title },
+  computed: {
+    account() {
+      return this.$store.getters["user/account"];
+    },
+    factionOptions() {
+      return [{ label: 'The Corporations', value: 'corp' }, { label: 'The Degenerates', value: 'degen' }];
+    }
+  },
+  data() {
+    return {
+      avatar: '',
+      faction: "corp"
+    }
+  }
+};
+</script>
+<style lang="scss" scoped>
+.background {
+  background-size: cover;
+  background-position: top;
+  height: 100vh;
+  background-image: url("/images/background/post.png");
+}
+
+.screen-container {
+  background: linear-gradient(0deg, #181a1b, #181a1b), #3a2720;
+  border: 2px solid #ffeebc;
+  box-sizing: border-box;
+}
+
+.avatar {
+  height: 260px;
+  width: 260px;
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -26,6 +26,7 @@ import LoyaltyProgram from "@/pages/Loyalty/Program.vue";
 import LoyaltyShop from "@/pages/Loyalty/Shop.vue";
 import StartSwap from "@/pages/StartSwap.vue";
 import Soldier from "@/pages/Soldier.vue";
+import Account from "@/pages/Account/Account";
 
 Vue.use(Router);
 
@@ -160,6 +161,11 @@ export const routes = [
         path: '/unlock-soldier',
         name: 'unlock-soldier',
         component: Soldier
+      },
+      {
+        path: '/profile',
+        name: 'profile',
+        component: Account,
       },
       {
         path: "/farms",
