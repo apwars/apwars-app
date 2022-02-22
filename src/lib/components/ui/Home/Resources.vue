e82c0337bbbbb83acf52af6c51b79ee60b1f1dfc
dmenethil <dmenethil@protonmail.com>
2022-02-22 18:31:39 -0300

    Buy buttons

--- src/lib/components/ui/Home/Resources.vue
+++ src/lib/components/ui/Home/Resources.vue
@@ -8,6 +8,7 @@
         APWars Off-chain
       </div>
       <div class="resource-row mt-1">
+        <div class="token-info">
         <div class="token-container">
           <v-img src="/images/wgold.png" />
         </div>
@@ -15,27 +16,34 @@
           <Amount :amount="getBalance('wGOLD')" />
           <div class="token-name">wGOLD</div>
         </div>
-        <div class="buy-container"></div>
+        </div>
+        <div class="buy-container"><Button type="wsecondary" size="small" text="Buy wGOLD" isBlock :handleClick="() => $router.push('/buy-wgold')" /></div>
       </div>
       <div class="resource-row">
+        <div class="token-info">
         <div class="token-container">
           <img src="/images/wcourage.png" width="24px" />
         </div>
+        
         <div class="balance-info">
           <Amount :amount="getBalance('wCOURAGE')" />
           <div class="token-name">wCOURAGE</div>
         </div>
-        <div class="buy-container"></div>
+        </div>
+        <div class="buy-container"><Button type="wsecondary" size="small" text="Buy wCOURAGE" isBlock :handleClick="() => $router.push('/buy-wcourage')" /></div>
       </div>
       <div class="resource-row">
+        <div class="token-info">
         <div class="token-container">
           <img src="/images/wland.png" width="24px" />
         </div>
+        
         <div class="balance-info">
           <Amount :amount="getBalance('wLAND')" />
           <div class="token-name">wLAND</div>
         </div>
-        <div class="buy-container"></div>
+        </div>
+        <div class="buy-container"><Button type="wsecondary" size="small" text="Buy wLAND" isBlock :handleClick="() => $router.push('/buy-wland')" /></div>
       </div>
     </v-col>
   </v-row>
@@ -43,11 +51,12 @@
 <script>
 import { mapState } from "vuex";
 
+import Button from "@/lib/components/ui/Buttons/Button";
 import Toggle from "@/lib/components/ui/Toggle";
 import Amount from "@/lib/components/ui/Utils/Amount";
 
 export default {
-  components: { Amount, Toggle },
+  components: { Button, Amount, Toggle },
   computed: {
     ...mapState({
       onChainBalance: (state) => state.wallet.onChainBalance,
@@ -86,6 +95,7 @@ export default {
 }
 .resource-row {
   display: flex;
+  justify-content: space-between;
   align-items: center;
   margin-bottom: 8px;
 }
@@ -108,6 +118,10 @@ export default {
   justify-content: center;
   align-items: center;
 }
+.token-info {
+  display: flex;
+  align-content: center;
+}
 .balance-info {
   margin-left: 4px;
   font-weight: bold;
@@ -117,5 +131,8 @@ export default {
 .token-name {
   font-size: 10px;
   line-height: 1.3;
+}
+.buy-container {
+  width: 140px;
 }
 </style>
