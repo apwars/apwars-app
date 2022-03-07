cbe273bdf0ed196bced816da6fbd1f589e9e1b9a
dmenethil <dmenethil@protonmail.com>
2022-03-07 18:11:39 -0300

    Format form

--- src/pages/Account/Account.vue
+++ src/pages/Account/Account.vue
@@ -1,87 +1,122 @@
-<template
-  ><div class="background">
+<template>
+  <div class="background">
     <v-container>
       <v-row>
         <v-col><Title>Account</Title></v-col></v-row
       >
       <v-row>
         <v-col>
-          <v-row class="screen-container">
-            <v-col cols="12" md="4"
-              ><v-select
-                v-model="faction"
-                item-text="label"
-                item-value="value"
-                :items="factionOptions"
-              />
-              <v-text-field v-model="avatar" />
-
-              <v-select
-                v-model="country"
-                item-text="name"
-                item-value="value"
-                :items="countryOptions"
-              >
-                <template v-slot:append-outer>
-                  <img
-                    :src="`/images/country-flags/${country}.svg`"
-                    width="64px"
-                    :alt="country"
-                  />
-                </template>
-              </v-select>
-              <v-radio-group v-model="isPublic" row>
-                <v-radio label="Yes" :value="true"></v-radio>
-                <v-radio label="No" :value="false"></v-radio>
-              </v-radio-group>
-            </v-col>
-
-            <v-col cols="12" md="4">
-              <img
-                class="avatar"
-                :src="
-                  `https://avatar.apwars.farm/?seed=${avatar ||
-                    account}&avatar=${faction}`
-                "
-                alt="avatar"
-              />
-              <div class="lp-level-container">
-                You are Level 1 on the Loyalty Program
-              </div></v-col
-            ><v-col cols="12" md="4">
-              <v-row>
-                <v-col>
-                  <div class="d-flex">
-                    <img
-                      src="/images/icons/coins/wSCARS.png"
-                      width="95px"
-                      alt="War SCARS"
+          <v-row>
+            <v-col>
+              <div class="screen-container">
+                <v-row>
+                  <v-col cols="12" md="4">
+                    <v-text-field v-model="avatar" label="Name Warrior" />
+                    <v-select
+                      v-model="faction"
+                      item-text="label"
+                      item-value="value"
+                      :items="factionOptions"
+                      label="Faction"
                     />
-                    <div class="scars-info">
-                      <div class="scars">75,000</div>
-                      <div class="name">War Scars</div>
-                    </div>
-                  </div>
-                </v-col></v-row
-              >
-              <v-row>
-                <v-col>wCOURAGE</v-col>
-                <v-col>wGOLD</v-col>
-                <v-col>wLAND</v-col>
-              </v-row>
-            </v-col></v-row
-          >
+
+                    <v-select
+                      v-model="country"
+                      item-text="name"
+                      item-value="value"
+                      :items="countryOptions"
+                      label="Country"
+                    >
+                      <template v-slot:append-outer>
+                        <img
+                          :src="`/images/country-flags/${country}.svg`"
+                          width="64px"
+                          :alt="country"
+                        />
+                      </template>
+                    </v-select>
+
+                    <div class="radio-label">Is Public?</div>
+                    <v-radio-group v-model="isPublic" row>
+                      <v-radio label="Yes" :value="true"></v-radio>
+                      <v-radio label="No" :value="false"></v-radio>
+                    </v-radio-group>
+                  </v-col>
+
+                  <v-col cols="12" md="4">
+                    <img
+                      class="avatar"
+                      :src="
+                        `https://avatar.apwars.farm/?seed=${avatar ||
+                          account}&avatar=${faction}`
+                      "
+                      alt="avatar"
+                    />
+                    <div class="lp-level-container">
+                      <div class="level-image">
+                        <v-img src="/images/lp-levels/1.png" />
+                      </div>
+                      <div class="level-text">
+                        You are Level 1 on the Loyalty Program
+                      </div>
+                    </div></v-col
+                  ><v-col cols="12" md="4">
+                    <v-row>
+                      <v-col>
+                        <div class="d-flex">
+                          <img
+                            src="/images/icons/coins/wSCARS.png"
+                            width="95px"
+                            alt="War SCARS"
+                          />
+                          <div class="scars-info">
+                            <div class="scars">75,000</div>
+                            <div class="name">War Scars</div>
+                          </div>
+                        </div>
+                      </v-col></v-row
+                    >
+                    <v-row>
+                      <v-col>wCOURAGE</v-col>
+                      <v-col>wGOLD</v-col>
+                      <v-col>wLAND</v-col>
+                    </v-row>
+                  </v-col>
+                </v-row>
+              </div>
+            </v-col>
+          </v-row>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col>
+          <div class="actions-container">
+            <Button
+              type="wsecondary"
+              text="Save"
+              v-if="isEditing"
+              :handleClick="toggleMode"
+            />
+            <Button
+              type="wsecondary"
+              text="Edit"
+              v-else
+              :handleClick="toggleMode"
+            />
+          </div>
         </v-col>
       </v-row>
     </v-container>
-  </div></template
->
+  </div>
+</template>
 <script>
 import { countryOptions } from "@/data/Countrys";
+
+import Button from "@/lib/components/ui/Buttons/Button";
 import Title from "@/lib/components/ui/Title";
 
 export default {
-  components: { Title },
+  components: { Button, Title },
   computed: {
     account() {
       return this.$store.getters["user/account"];
@@ -98,11 +133,17 @@ export default {
   },
   data() {
     return {
+      isEditing: false,
       avatar: "",
       faction: "corp",
       country: "br",
       isPublic: true,
     };
+  },
+  methods: {
+    toggleMode() {
+      this.isEditing = !this.isEditing;
+    },
   },
 };
 </script>
@@ -115,6 +156,7 @@ export default {
 }
 
 .screen-container {
+  padding: 24px;
   background: linear-gradient(0deg, #181a1b, #181a1b), #3a2720;
   border: 2px solid #ffeebc;
   box-sizing: border-box;
@@ -130,10 +172,23 @@ export default {
 }
 
 .lp-level-container {
-  @extend .highlight-color;
-  font-weight: bold;
-  font-size: 16px;
-  line-height: 1.3;
+  display: flex;
+  align-items: center;
+  max-width: 260px;
+  .level-image {
+    width: 64px;
+  }
+  .level-text {
+    @extend .highlight-color;
+    font-weight: bold;
+    font-size: 16px;
+    line-height: 1.3;
+  }
+}
+
+.radio-label {
+  display: block;
+  width: 100%;
 }
 
 .scars-info {
@@ -150,5 +205,10 @@ export default {
     line-height: 1.4;
     color: #ffffff;
   }
+}
+.actions-container {
+  width: 100%;
+  display: flex;
+  justify-content: flex-end;
 }
 </style>
